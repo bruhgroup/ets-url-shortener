140e9eadff1e0df4af11ab4a6ad3fb04507adf8e
echung32 <143231894+echung32@users.noreply.github.com>
2022-10-25 13:53:26 -1000

    feat: add styling to create account

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -18,11 +18,11 @@ export default function Authentication() {
     if (loading) {
         return <p>loading auth state...</p>;
     }
-
     if (user) {
         return (<Navigate to="/dashboard"/>)
     }
 
+    // TODO: Move these into components
     return (
         register ?
             <Create setRegister={setRegister}/> :
@@ -54,25 +54,37 @@ function Create({setRegister}: { setRegister: Dispatch<boolean> }) {
         );
     }
     return (
-        <div className="App">
-            <label htmlFor="email"> email </label>
-            <input
-                className={"border-2 border-solid border-black rounded"}
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="password">password </label>
-            <input
-                className={"border-2 border-solid border-black rounded"}
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={() => createUserWithEmailAndPassword(email, password)}>
-                Register
-            </button>
-            <button onClick={() => setRegister(false)}>Already have an account? Log in.</button>
+        <div className={"flex flex-col gap-4 bg-white p-8 justify-center rounded max-w-full w-[36em]"}>
+            <div className={"flex justify-center"}>
+                <span className={"font-medium text-2xl mb-[24px]"}>Authentication</span>
+            </div>
+            <form
+                className={"flex flex-col gap-4 m-auto max-w-full w-[22em]"}
+                onSubmit={() => createUserWithEmailAndPassword(email, password)}
+            >
+                <label htmlFor={"email"}>Email</label>
+                <input
+                    id={"email"}
+                    className={"flex p-2 border-2 border-c-gray-300 rounded"}
+                    placeholder={"example@email.com"}
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    type={"email"}
+                />
+                <label htmlFor={"password"}>Password</label>
+                <input
+                    id={"password"}
+                    className={"flex p-2 border-2 border-c-gray-300 rounded"}
+                    placeholder={"********"}
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    type={"password"}
+                />
+                <div className={"max-h-[45px] rounded px-[16px] py-[8px] bg-blue-500 font-medium w-full text-center"}>
+                    <button className={"text-white"} type={"submit"}>Create Account</button>
+                </div>
+            </form>
+            <button className={"text-gray-500"} onClick={() => setRegister(false)}>Already have an account?</button>
         </div>
     );
 }
@@ -132,7 +144,7 @@ function Login({setRegister}: { setRegister: Dispatch<boolean> }) {
                     <button className={"text-white"} type={"submit"}>Login</button>
                 </div>
             </form>
-            <button onClick={() => setRegister(true)}>Don't have an account?</button>
+            <button className={"text-gray-500"} onClick={() => setRegister(true)}>Don't have an account?</button>
         </div>
     );
 }
