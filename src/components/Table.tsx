8333adcc803c4a4d8fd7bb1172dd169d6c7b1eba
eve-liya <vincenthawaii4125@gmail.com>
2022-10-26 01:05:07 -1000

    feat: table styles

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -19,17 +19,24 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
             columnHelper.display({
                 id: 'actions',
                 cell: (props) =>
-                    <button className={"bg-red-600 rounded-2xl w-6 h-6"}
+                    <button className={"bg-red-600 rounded-2xl w-6 h-6 mx-2"}
                             onClick={() => removeData(userid, `${props.row.getVisibleCells()[1].getValue() ?? ""}`)}/>
             }),
             columnHelper.accessor("short", {
                 cell: info => info.getValue(),
+                header: "Short"
             }),
             columnHelper.accessor("long", {
-                cell: info => info.getValue(),
+                size: 10,
+                cell: (props) => <p
+                className={"truncate w-[32rem]"}
+                >{props.getValue()}</p>,
+                header: "Original"
             }),
             columnHelper.accessor("desc", {
+                id: 'description',
                 cell: info => info.getValue(),
+                header: "Description"
             })
         ], [userid])
 
@@ -45,18 +52,28 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
 
     return (
         <div className="p-2">
-            <table>
-                <thead>
+            <table
+            className={"shadow"}>
+                <thead
+                className={"bg-gray-200"}
+                >
                 {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
+                    <tr
+                        className={""}
+                        key={headerGroup.id}>
                         {headerGroup.headers.map(header => (
-                            <th key={header.id} colSpan={header.colSpan}>
+                            <th key={header.id} colSpan={header.colSpan}
+                            className={
+                                `${(header.id === "actions" ? "rounded-tl-md" : header.id === "description" ? "rounded-tr-md" : "")}
+                                text-left px-2`
+                            }
+                            >
                                 {header.isPlaceholder ? null : (
                                     <div
-                                        className={
-                                            header.column.getCanSort()
+                                        className={`
+                                            ${header.column.getCanSort()
                                                 ? 'cursor-pointer select-none'
-                                                : ''
+                                                : ''} my-3`
                                         }
                                         onClick={header.column.getToggleSortingHandler()}>
                                         {flexRender(
@@ -76,9 +93,11 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
                 </thead>
                 <tbody>
                 {table.getRowModel().rows.map(row => (
-                    <tr key={row.id}>
+                    <tr key={row.id}
+                        className={"odd:bg-white even:bg-gray-100"}>
                         {row.getVisibleCells().map(cell => (
-                            <td key={cell.id}>
+                            <td key={cell.id}
+                            >
                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
                             </td>
                         ))}
