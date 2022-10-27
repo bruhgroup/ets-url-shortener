057a81b81a661c15a65ce8ce45b8ff9e8f3baa5e
echung32 <143231894+echung32@users.noreply.github.com>
2022-10-26 16:14:03 -1000

    fix: attempt to fix table styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -19,6 +19,7 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
         () => [
             columnHelper.display({
                 id: "settings",
+                size: 50,
                 cell: props =>
                     <button
                         className={"p-4"}
@@ -28,28 +29,33 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
             }),
             columnHelper.accessor("short", {
                 header: "Short",
-                cell: info => info.getValue(),
+                size: 100,
+                cell: info => <span className={"max-w-[100px]"}>{info.getValue()}</span>,
             }),
             columnHelper.accessor("long", {
                 header: "Original",
-                cell: info => info.getValue(),
+                size: 150,
+                cell: info => <span className={"max-w-[150px]"}>{info.getValue()}</span>,
             }),
             columnHelper.accessor("desc", {
                 header: "Description",
-                cell: info => info.getValue(),
+                size: 250,
+                cell: info => <span className={"max-w-[250px]"}>{info.getValue()}</span>,
             }),
             columnHelper.display({
                 header: "QR Code",
+                size: 100,
                 cell: props =>
-                    <button className={""} onClick={() => {
+                    <button className={"max-w-[100px] w-[100px]"} onClick={() => {
                     }}>
                         COPY
                     </button>
             }),
             columnHelper.display({
                 header: "URL",
+                size: 100,
                 cell: props =>
-                    <button className={""} onClick={() => {
+                    <button className={"max-w-[100px] w-[100px]"} onClick={() => {
                     }}>
                         COPY
                     </button>
@@ -62,24 +68,22 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
         state: {sorting},
         onSortingChange: setSorting,
         getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        debugTable: true,
+        getSortedRowModel: getSortedRowModel()
     })
 
     return (
-        <div className="overflow-x-auto rounded">
-            <table className={"table-auto min-w-max"}>
+        <div className="overflow-x-scroll rounded hide-scrollbar">
+            <table className={"min-w-max"}>
                 <thead className={"bg-gray-200"}>
                 {table.getHeaderGroups().map(headerGroup => (
                     <tr
                         key={headerGroup.id}>
                         {headerGroup.headers.map(header => (
                             <th key={header.id} colSpan={header.colSpan}
-                                className={"text-left px-2"}
-                            >
+                                className={"text-left px-2"}>
                                 {header.isPlaceholder ? null : (
                                     <div
-                                        className={`my-3 ${header.column.getCanSort() ? 'cursor-pointer select-none' : ''}`}
+                                        className={`my-3 ${header.column.getCanSort() ? "cursor-pointer select-none" : ""}`}
                                         onClick={header.column.getToggleSortingHandler()}>
                                         {flexRender(
                                             header.column.columnDef.header,
@@ -101,7 +105,8 @@ export default function Table({links, userid}: { links: LinkData[], userid: stri
                     <tr key={row.id}
                         className={"odd:bg-white even:bg-gray-100"}>
                         {row.getVisibleCells().map(cell => (
-                            <td key={cell.id} className={"text-left px-2"}>
+                            <td key={cell.id}
+                                className={`text-left px-2 overflow-hidden overflow-x-scroll hide-scrollbar max-w-[${cell.column.getSize()}px]`}>
                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
                             </td>
                         ))}
