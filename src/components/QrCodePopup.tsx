1126bacf78d037f16f1c600d0251234b4e384c66
eve-liya <vincenthawaii4125@gmail.com>
2022-10-30 18:41:06 -1000

    feat: QR code copy

--- src/components/QrCodePopup.tsx
+++ src/components/QrCodePopup.tsx
@@ -3,14 +3,30 @@ import Popup from "reactjs-popup";
 
 export default function QrCodePopup({url, id}: {url:string, id:string}){
     function download(){
-        console.log("ran")
         let canvas: HTMLCanvasElement | null = document.querySelector("#i" + id)
         let anchor = document.createElement("a")
         if (canvas == null) return
-        console.log("should download")
+        console.log("Downloading png")
         anchor.href = canvas.toDataURL("image/png")
         anchor.download = `${id}.PNG`
         anchor.click()
+    }
+    async function copy() {
+        let canvas: HTMLCanvasElement | null = document.querySelector("#i" + id)
+        if (canvas == null) return
+        const blob = await imageToBlob(canvas)
+        const item = new ClipboardItem({ "image/png": blob });
+        await navigator.clipboard.write([item]);
+    }
+
+    function imageToBlob(canvas:HTMLCanvasElement) {
+        return new Promise<Blob>(resolve => {
+                canvas.toBlob((blob) => {
+                    // here the image is a blob
+                    if (blob == null) return;
+                    resolve(blob)
+                }, "image/png", 0.75);
+        })
     }
 
     return (
@@ -29,6 +45,7 @@ export default function QrCodePopup({url, id}: {url:string, id:string}){
                 <div className={"flex-row text-center bg-white "}>
                     <button
                         className={"mx-1 bg-blue-400 rounded py-1 px-2"}
+                        onClick={()=> copy()}
                     >Copy</button>
                     <button
                         className={"mx-1 bg-blue-400 rounded py-1 px-2"}
